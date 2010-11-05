48715d35f209f2de5344a9125cb551f8665e0ae0
Eric Davis <edavis@littlestreamsoftware.com>
2010-11-04 20:44:33 -0700

    Add list_books script

---
+++ bin/list_books.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'kindle'
+
+unless ARGV[0] && File.exists?(ARGV[0])
+  puts "ERROR: need file"
+  exit(-1)
+end
+
+kindle_file = Kindle::File.open(ARGV[0]).parse
+kindle_file.books.each do |book|
+  puts book.title + " with #{book.notes.length} notes"
+end
+
