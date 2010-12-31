de6b0b23bcc19174b835144f16cecc3bdd208f49
Eric Davis <edavis@littlestreamsoftware.com>
2010-12-31 08:48:30 -0800

    Remove the author from the title attribute

--- lib/kindle/book_title.rb
+++ lib/kindle/book_title.rb
@@ -2,7 +2,15 @@ module Kindle
   class BookTitle
     def self.from_kindle_format(note)
       title_author, *notes = *note.split("\r\n")
-      title_author
+      # "Title (something optional) (Author)" =>
+      #   "Title (somethine optional)"
+      title_match = title_author.match(/(.*)\(.*\)$/)
+      if title_match
+        title = title_match[1].strip
+      else
+        title = ''
+      end
+      title
     end
   end
 end
